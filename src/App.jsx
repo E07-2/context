d87fb905793e0665f169b4f1391e7529deb4e080
ion-dci <ion.lazarev@digitalcareerinstitute.org>
2022-02-17 10:21:34 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-// import {useContext} from 'react';
-// import MyContext from './context/MyContext';
 import "./App.css";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 
 const App = () => {
-  // const context = useContext(MyContext)
-  // console.log(context)
+ 
   return (
     <main>
       <Header />
